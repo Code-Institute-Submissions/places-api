0e98059d8c4c11da71c27c1935be79eb7182bfcd
toader <toaderchirila21@gmail.com>
2019-05-05 07:12:15 +0000

    Values added for different type (html) and first 2 values added in the for loop js

--- assets/js/map.js
+++ assets/js/map.js
@@ -52,9 +52,10 @@ function onPlaceChanged() {
 }
 // Search for places in the selected city, within the viewport of the map.
 function search() {
+    var selectType = document.getElementById("selectType");
     var search = {
         bounds: map.getBounds(),
-        types: ['bank']
+        types: [selectType]
     // loop for different places goes down here 
     };
     places.nearbySearch(search, function(results, status) {
@@ -64,8 +65,17 @@ function search() {
             // Create a marker for each place found, and
             // assign a letter of the alphabetic to each marker icon.
             for (var i = 0; i < results.length; i++) {
-                var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-                var markerIcon = MARKER_PATH + markerLetter + '.png';
+               if (selectType == "museum") {
+                   // Icon??
+               }
+               else if (selectType == "zoo") {
+                   // icon??
+               }
+               else if (selectType == "amusement_park") {
+                   
+               }
+               
+               
                 // Use marker animation to drop the icons incrementally on the map.
                 markers[i] = new google.maps.Marker({
                     position: results[i].geometry.location,
