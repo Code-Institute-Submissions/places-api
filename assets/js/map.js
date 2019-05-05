03347736aed465787bc8c287d6d1aca863c65321
toader <toaderchirila21@gmail.com>
2019-05-05 07:22:13 +0000

    bug----- search type does not work ....(hotels returned for some reason) no errors in the console

--- assets/js/map.js
+++ assets/js/map.js
@@ -75,7 +75,8 @@ function search() {
                    
                }
                
-               
+               var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+               var markerIcon = MARKER_PATH + markerLetter + '.png';
                 // Use marker animation to drop the icons incrementally on the map.
                 markers[i] = new google.maps.Marker({
                     position: results[i].geometry.location,
