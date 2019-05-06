47231af25fd8a68f37ebae512d5ce393f882159e
toader <toaderchirila21@gmail.com>
2019-05-06 07:02:01 +0000

    listener added to clear the markers and results table on type change

--- assets/js/map.js
+++ assets/js/map.js
@@ -22,23 +22,23 @@ function initMap() {
         gestureHandling: 'cooperative'
     });
 
-infoWindow = new google.maps.InfoWindow({
-    content: document.getElementById('info-content')
-});
-
-// Create the autocomplete object and associate it with the UI input control.
-
-autocomplete = new google.maps.places.Autocomplete(
-    /** @type {!HTMLInputElement} */
-    (
-        document.getElementById('autocomplete')), {
-        types: ['(cities)']
+    infoWindow = new google.maps.InfoWindow({
+        content: document.getElementById('info-content')
     });
-places = new google.maps.places.PlacesService(map);
 
-// Add a DOM event listener to react when the user selects a new place.
+    // Create the autocomplete object and associate it with the UI input control.
 
-autocomplete.addListener('place_changed', onPlaceChanged);
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */
+        (
+            document.getElementById('autocomplete')), {
+            types: ['(cities)']
+        });
+    places = new google.maps.places.PlacesService(map);
+
+    // Add a DOM event listener to react when the user selects a new place.
+
+    autocomplete.addListener('place_changed', onPlaceChanged);
 }
 // Search for places in the selected city, within the viewport of the map.
 function search() {
@@ -46,8 +46,8 @@ function search() {
     var search = {
         bounds: map.getBounds(),
         types: [selectType],
-        
-    // loop for different places goes down here 
+
+        // loop for different places goes down here 
     };
     places.nearbySearch(search, function(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -56,22 +56,19 @@ function search() {
             // Create a marker for each place found, and
             // assign a letter of the alphabetic to each marker icon.
             for (var i = 0; i < results.length; i++) {
-               if (selectType == 'museum') {
-                   MARKER_PATH = "http://cdn.onlinewebfonts.com/svg/img_465542.png"
-               }
-               else if (selectType == 'zoo') {
-                   // icon??
-               }
-               else if (selectType == "amusement_park") {
-                   
-               }
-               
-               document.getElementById("selectType").addEventListener('change', clearMarkers);
-               document.getElementById("selectType").addEventListener('change', clearResults);
-               
-               
-               var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-               var markerIcon = MARKER_PATH + markerLetter + '.png';
+                if (selectType == 'museum') {
+                    markerIcon = "http://cdn.onlinewebfonts.com/svg/img_465542.png"
+                }
+                else if (selectType == 'zoo') {
+                    // icon??
+                }
+                else if (selectType == "amusement_park") {
+
+                }
+
+
+                var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+                var markerIcon = MARKER_PATH + markerLetter + '.png';
                 // Use marker animation to drop the icons incrementally on the map.
                 markers[i] = new google.maps.Marker({
                     position: results[i].geometry.location,
@@ -88,7 +85,10 @@ function search() {
         }
     });
 }
-   document.getElementById("selectType").addEventListener('change', search);
+document.getElementById("selectType").addEventListener('change', clearMarkers);
+document.getElementById("selectType").addEventListener('change', clearResults);
+document.getElementById("selectType").addEventListener('change', search);
+
 function onPlaceChanged() {
     var place = autocomplete.getPlace();
     if (place.geometry) {
